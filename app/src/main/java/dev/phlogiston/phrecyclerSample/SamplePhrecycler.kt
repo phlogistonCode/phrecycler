9a3b1251c35872b58236acbfd6e94a8b34023d4c
phlogiston <alexandr.popov.95@gmail.com>
2020-04-05 17:24:40 +0400

    [fix] clicks views

--- app/src/main/java/dev/phlogiston/phrecyclerSample/SamplePhrecycler.kt
+++ app/src/main/java/dev/phlogiston/phrecyclerSample/SamplePhrecycler.kt
@@ -5,7 +5,7 @@ import dev.phlogiston.phrecycler.PhrecyclerAdapter
 class SamplePhrecycler(private val click: (DataClass) -> Unit,
                        click1: (DataClass) -> Unit,
                        click2: (DataClass) -> Unit,
-                       click3: (DataClass) -> Unit) : PhrecyclerAdapter<DataClass>() {
+                       click3: (DataClass) -> Unit) : PhrecyclerAdapter<DataClass>(click1, click2, click3) {
 
     override val setViewHolders = mapOf(
         Item1VH::class.java to R.layout.item_1,
